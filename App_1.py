46fd94c26414febbf449ec6c3a932ec8252a1703
Nikita Belov <lolvvto@yandex.ru>
2022-12-26 15:36:21 +0700

    Add App 1 and input 1

---
+++ App_1.py
@@ -0,0 +1,11 @@
+# # 1. Вычислить число c заданной точностью d
+# Пример:
+# - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
+
+import math
+
+with open('input_1.txt', 'r', encoding='utf-8') as file:
+    d = file.readline()
+    sim = len(str(d).split('.')[1])
+    pi = math.pi
+    print('При d =', d , 'pi =', round(pi, sim))
